bad0a479a5f6357325d1b6eda8599e78dbb07e55
Howard Rauscher <howardr@gmail.com>
2011-09-07 14:41:21 -0500

    more network context handling

--- tweetriver.handlebars.js
+++ tweetriver.handlebars.js
@@ -9,10 +9,17 @@ if(massrel.handlebars) {
 function register(Handlebars) {
 
   Handlebars.registerHelper('autoLink', function(text) {
-    return twttr.txt.autoLink(text, { target: '_blank', urlEntities: this.status.entities.urls });
-  });
+    var options = { target: '_blank' };
 
-  
+    if(this.source.twitter) {
+      if(this.status.entities && this.status.entities.urls) {
+        options.urlEntities = this.status.entities.urls;
+      }
+      return twttr.txt.autoLink(text, options);
+    }
+
+    return twttr.txt.autoLinkUrlsCustom(text, options);
+  });
 
   Handlebars.registerHelper('prettyDate', function(date) {
     date = massrel.helpers.fix_twitter_date(date);
@@ -21,7 +28,6 @@ function register(Handlebars) {
 
 }
 
-
 function prepare_context(status, opts) {
   if(status.retweeted_status) {
     var context = massrel.handlebars.prepare_context(status.retweeted_status, opts);
@@ -29,15 +35,30 @@ function prepare_context(status, opts) {
     context.retweeted_by_user = status.user;
     return context;
   }
-
+  
   opts = massrel.helpers.extend(opts || {}, {
     intents: true
   });
 
   var context = {
     status: status,
-    intents: opts.intents
+    intents: opts.intents,
+    source: {
+      facebook: false,
+      twitter: false
+    },
+    known: true
   };
+
+  // determine status source
+  if(status.facebook_id) {
+    context.source.facebook = true;
+    context.known = (typeof(status.message) === 'string');
+  }
+  else {
+    context.source.twitter = true;
+  }
+
   return context;
 };
 
