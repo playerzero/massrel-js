3d8cb6a476f47ec31e1bd2739fafe15f238cd7a7
Howard Rauscher <howardr@gmail.com>
2012-02-02 13:53:21 -0600

    tests for Stream param builder methods

--- test/spec/Stream.spec.js
+++ test/spec/Stream.spec.js
@@ -18,6 +18,53 @@ describe('Stream', function() {
 expect(stream.meta_url()).toEqual('http://tweetriver.com/howardrauscher/test/meta.json');
   });
 
+  it('use correct params from options', function() {
+    var stream = new massrel.Stream('howardrauscher/test');
+    var params;
+
+    params = stream.buildParams();
+    expect(params.length).toEqual(0);
+
+    params = stream.buildParams({});
+    expect(params.length).toEqual(0);
+
+    var testParam = function(opts, key, val) {
+      var params = stream.buildParams(opts);
+      expect(params.length).toEqual(1);
+      expect(params[0][0]).toEqual(key);
+      expect(params[0][1]).toEqual(val);
+    };
+
+    testParam({ limit: 5 }, 'limit', 5);
+    testParam({ since_id: '100001' }, 'since_id', '100001');
+    testParam({ start: '10001' }, 'start', '10001');
+    testParam({ start_id: '10001' }, 'start', '10001');
+    testParam({ since_id: '100001', start_id: '20002' }, 'since_id', '100001');
+    testParam({ replies: true }, 'replies', '1');
+    testParam({ geo_hint: true }, 'geo_hint', '1');  
+  });
+
+  it('use correct params from meta options', function() {
+    var stream = new massrel.Stream('howardrauscher/test');
+    var params;
+
+    params = stream.buildParams();
+    expect(params.length).toEqual(0);
+
+    params = stream.buildParams({});
+    expect(params.length).toEqual(0);
+
+    var testParam = function(opts, key, val) {
+      var params = stream.builMetaParams(opts);
+      expect(params.length).toEqual(1);
+      expect(params[0][0]).toEqual(key);
+      expect(params[0][1]).toEqual(val);
+    };
+
+    testParam({ disregard: 'word' }, 'disregard', 'word');
+  });
+
+
   //TODO: #load, #each, #poller, #meta
 
 });
