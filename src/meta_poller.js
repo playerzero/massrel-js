405ed8de0deeab76ec91c74a459a1083a788d1d1
Howard Rauscher <howardr@gmail.com>
2012-07-13 11:10:28 -0500

    Revert generic poller code. It was supposed to be in a branch

--- src/meta_poller.js
+++ src/meta_poller.js
@@ -1,16 +1,53 @@
-define(['helpers', 'generic_poller'], function(helpers, GenericPoller) {
+define(['helpers'], function(helpers) {
 
-  function MetaPoller() {
-    GenericPoller.apply(this, arguments);
+  function MetaPoller(object, opts) {
+    var self = this
+      , fetch = function() {
+          if(enabled) {
+            object.meta(self.opts, function(data) { // success
+              if(enabled) { // being very thorough in making sure to stop polling when told
+                helpers.step_through(data, self._listeners, self);
+
+                if(enabled) { // poller can be stopped in any of the above iterators
+                  again();
+                }
+              }
+            }, function() { // error
+              again();
+            });
+          }
+        }
+      , again = function() {
+          tmo = setTimeout(fetch, helpers.poll_interval(self.opts.frequency));
+        }
+      , enabled = false
+      , tmo;
+
+    this._listeners = [];
+
+    this.opts = opts || {};
+    
+    this.opts.frequency = (this.opts.frequency || 30) * 1000;
+
+    this.start = function() {
+      if(!enabled) { // guard against multiple pollers
+        enabled = true;
+        fetch();
+      }
+      return this;
+    };
+    this.stop = function() {
+      clearTimeout(tmo);
+      enabled = false;
+      return this;
+    };
   }
 
-  helpers.extend(MetaPoller.prototype, GenericPoller.prototype);
-
-  MetaPoller.prototype.fetch = function(object, options, skip, callback, error) {
-    object.meta(options, callback, error);
+  MetaPoller.prototype.data = function(fn) {
+    this._listeners.push(fn);
     return this;
   };
-
+  // alias #each
   MetaPoller.prototype.each = MetaPoller.prototype.data;
 
   return MetaPoller;
--- src/poller.js
+++ src/poller.js
@@ -1,92 +1,116 @@
-define(['helpers', 'generic_poller', 'poller_queue'], function(helpers, GenericPoller, PollerQueue) {
+define(['helpers', 'poller_queue'], function(helpers, PollerQueue) {
 
-  function Poller() {
-    GenericPoller.apply(this, arguments);
-
-    this.since_id = this.opts.since_id || null;
-    this.start_id = this.opts.start_id || null;
-    this.stay_realtime = 'stay_realtime' in this.opts ? !!this.opts.stay_realtime : true;
+  function Poller(stream, opts) {
+    this.stream = stream;
+    this._callbacks = [];
+    this._enumerators = [];
+    this._bound_enum = false;
+    this._t = null;
+    
+    opts = opts || {};
+    this.limit = opts.limit || null;
+    this.since_id = opts.since_id || null;
+    this.start_id = opts.start_id || null;
+    this.replies = !!opts.replies;
+    this.geo_hint = !!opts.geo_hint;
+    this.keywords = opts.keywords || null;
+    this.frequency = (opts.frequency || 30) * 1000;
+    this.stay_realtime = 'stay_realtime' in opts ? !!opts.stay_realtime : true;
+    this.enabled = false;
+    this.alive = true;
+    this.alive_instance = 0;
+    this.consecutive_errors = 0;
   }
-
-  helpers.extend(Poller.prototype, GenericPoller.prototype);
-
-  // fetch data for Stream
-  Poller.prototype.fetch = function(object, opts, skip, callback, error) {
-    var self = this;
-    var load_opts = {
-      // prevents start_id from being include in query
-      start_id: null
-    };
-
-    // if in "stay realtime" mode then
-    // then poller should use "since_id"
-    //
-    // "since_id" gets statuses from the newest item in a stream
-    // to "since_id" (exclusive) or until "limit" is reached.
-    // statuses will be skipped in order to stay realtime
-    //
-    // "from_id" gets statuses from the provided id newer until
-    // the newest status in stream is found or until "limit" is reached.
-    // all statuses in a stream will be downloaded but it is likely
-    // to not stay realtime
-    if(this.stay_realtime) {
-      load_opts.since_id = self.since_id;
+  Poller.prototype.poke = function(fn) {
+    // this method should not be called externally...
+    // it basically restarts the poll loop if it stopped for network errors
+    // we call this if a request takes longer than 10sec
+    if(!this.alive && this.enabled) {
+      this._t = null;
+      this.start();
     }
-    else {
-      load_opts.from_id = self.since_id;
-    }
-
-    object.load(helpers.extend(load_opts, opts), function(statuses) {
-      // only invode hanlders is there are any statuses
-      // this is for legacy reasons
-      if(statuses && statuses.length > 0) {
-        self.since_id = statuses[0].entity_id;
-
-        if(!self.start_id) { // grab last item ID if it has not been set
-          self.start_id = statuses[statuses.length - 1].entity_id;
-        }
-      }
-      callback.apply(self, arguments);
-    }, error);
     return this;
   };
-
-  // Poller#batch callback will be invoked when there
-  // is an array of statuses that is > 0
-  // and be invoked with the entire array of statuses
-  Poller.prototype.each = function(fn) {
-    return this.data(Poller.createEnumerator(fn, false));
+  Poller.prototype.batch = function(fn) {
+    this._callbacks.push(fn);
+    return this;
   };
-
-  // Poller#each callback will be invoked when there
-  // is an array of statuses that is > 0
-  // and be invoked once for each item in the array
-  // from oldest status to newest statuse
   Poller.prototype.each = function(fn) {
-    return this.data(Poller.createEnumerator(fn, true));
+    this._enumerators.push(fn);
+    return this;
   };
+  Poller.prototype.start = function() {
+    if(this._t) {
+      return this;
+    }
+    this.enabled = true;
+    var instance_id = this.alive_instance = this.alive_instance + 1;
+    
+    var self = this;
+    function poll() {
+      self.alive = false;
 
-  // creates a new queue with the poller
-  Poller.prototype.queue = function() {
+      if(!self.enabled || instance_id !== self.alive_instance) { return; }
+
+      var load_opts = {};
+      if(self.stay_realtime) {
+        load_opts.since_id = self.since_id;
+      }
+      else {
+        load_opts.from_id = self.since_id;
+      }
+
+      self.stream.load(self.params(load_opts), function(statuses) {
+        self.alive = true;
+        self.consecutive_errors = 0;
+        
+        if(statuses && statuses.length > 0) {
+          self.since_id = statuses[0].entity_id;
+
+          if(!self.start_id) { // grab last item ID if it has not been set
+            self.start_id = statuses[statuses.length - 1].entity_id;
+          }
+          
+          // invoke all batch handlers on this poller
+          for(var i = 0, len = self._callbacks.length; i < len; i++) {
+            self._callbacks[i].call(self, statuses); // we might need to pass in a copy of statuses array
+          }
+          
+          // invoke all enumerators on this poller
+          helpers.step_through(statuses, self._enumerators, self);
+        }
+        self._t = setTimeout(poll, helpers.poll_interval(self.frequency));
+      }, function() {
+        self.consecutive_errors += 1;
+        self.poke();
+      });
+
+    }
+  
+    poll();
+    
+    return this;
+  };
+  Poller.prototype.stop = function() {
+    clearTimeout(this._t);
+    this._t = null;
+    this.enabled = false;
+    return this;
+  };
+  Poller.prototype.queue = function(fn) {
     var queue = new PollerQueue(this);
     queue.next(fn);
     return this;
   };
-
-  // gets "olders" statuses at bottom of stream
-  // and knows how to cursor down without getting duplicates
   Poller.prototype.more = function(fn, error) {
     //TODO: build in a lock, so multiple "more" calls
     //are called sequentially instead of in parallel
 
-    var self = this,
-        fetch = function() {
-          self.object.fetch(helpers.extend({
-            start_id: self.start_id,
-
-            // prevent since_id from being included in query
-            since_id: null
-          }, self.opts), function(statuses) {
+    var self = this
+      , fetch = function() {
+          self.stream.load(self.params({
+            start_id: self.start_id
+          }), function(statuses) {
             if(statuses.length > 0) {
               self.start_id = statuses[statuses.length - 1].entity_id;
               if(!self.since_id) {
@@ -107,33 +131,13 @@ define(['helpers', 'generic_poller', 'poller_queue'], function(helpers, GenericP
 
     return this;
   };
-
-  // legacy method that used to "kick start"
-  // a poller is there were network isssues
-  // I handle this at the request level now
-  // but leave as noop (basically) for now
-  Poller.prototype.poke = function() { return this; };
-
-  // creates an handler that will
-  // invoke the given handler once for each tweet in the response
-  // it will also only invoke given handler is there are 1 or more statuses (for legacy reasons)
-  Poller.createEnumerator = function(fn, enumerateEach) {
-    if(enumerateEach) {
-      return function(statuses) {
-        if(statuses && statuses.length > 0) {
-          // strep through will invote the handler (fn)
-          // from the oldest tweet (data.last) to the newest (data.first)
-          helpers.step_through(statuses, [fn], this);
-        }
-      };
-    }
-    else {
-      return function(statuses) {
-        if(statuses && statuses.length > 0) {
-          fn.apply(this, statuses);
-        }
-      }
-    }
+  Poller.prototype.params = function(opts) {
+    return helpers.extend({
+      limit: this.limit,
+      replies: this.replies,
+      geo_hint: this.geo_hint,
+      keywords: this.keywords
+    }, opts || {});
   };
 
   return Poller;
