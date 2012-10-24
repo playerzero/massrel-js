5fd729243b8dea79be6364489c3b7b9e73852865
Howard Rauscher <howardr@gmail.com>
2012-10-24 15:40:39 -0500

    auto set and allow customization of original_referer param in massrel.js

--- src/intents.js
+++ src/intents.js
@@ -3,20 +3,31 @@ define(['helpers'], function(helpers) {
   var intents = {
     base_url: 'https://twitter.com/intent/',
     params: {
-      'text'       : '(string): default text, for tweet/reply',
-      'url'        : '(string): prefill url, for tweet/reply',
-      'hashtags'   : '(string): hashtag (or list with ,) without #, for tweet/reply',
-      'related'    : '(string): screen name (or list with ,) without @, available for all',
-      'in_reply_to': '(number): tweet id, only for reply',
-      'via'        : '(string): screen name without @, tweet/reply',
-      'tweet_id'   : '(number): tweet id, for retweet and favorite',
-      'screen_name': '(string): only for user/profile',
-      'user_id'    : '(number): only for user/profile'
-    }
+      'text'            : '(string): default text, for tweet/reply',
+      'url'             : '(string): prefill url, for tweet/reply',
+      'hashtags'        : '(string): hashtag (or list with ,) without #, for tweet/reply',
+      'related'         : '(string): screen name (or list with ,) without @, available for all',
+      'in_reply_to'     : '(number): tweet id, only for reply',
+      'via'             : '(string): screen name without @, tweet/reply',
+      'tweet_id'        : '(number): tweet id, for retweet and favorite',
+      'screen_name'     : '(string): only for user/profile',
+      'user_id'         : '(number): only for user/profile',
+      'original_referer': '(string): url to display with related ("www.yahoo.com suggests you follow:")'
+    },
+    // set an original referer if the current page is
+    // iframed and there exists a referer
+    original_referer:  window.top !== window.self && document.referrer || null
   };
 
   intents.url = function(type, options) {
     options = options || {};
+
+    // automatically use the referer if user has not set one
+    // and we can safetly determine an original referer
+    if(options.original_referer === undefined && intents.original_referer) {
+      options.original_referer = intents.original_referer;
+    }
+
     var params = [];
     for(var k in options) {
       params.push([k, options[k]]);
