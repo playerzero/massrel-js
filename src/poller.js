12269c5ad2ef4c1678dd1951325177cb755f4131
Howard Rauscher <howardr@gmail.com>
2013-02-17 16:07:29 -0600

    fix global failure mode var

--- src/poller.js
+++ src/poller.js
@@ -124,7 +124,7 @@ define(['helpers', 'generic_poller', 'poller_queue'], function(helpers, GenericP
   // the poller is cursorable if no special
   // mode is in place
   Poller.prototype.cursorable = function() {
-    return !(Poller.failure_mode || this.failure_mode || this.hail_mary_mode);
+    return !(GenericPoller.failure_mode || this.failure_mode || this.hail_mary_mode);
   };
 
   Poller.prototype.filter_newer = function(statuses) {
