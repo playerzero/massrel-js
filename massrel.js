f088e2c7e0e3a3535d66c55b0f1480c2c51bee79
Ben Robbins <ben.robbins@gmail.com>
2013-08-06 17:47:06 -0500

    rake

--- massrel.js
+++ massrel.js
@@ -1691,11 +1691,13 @@ massreljs.define('context',['helpers'], function(helpers) {
     // flag the source in the map if it's a known source
     if (typeof context.source[status.network] !== 'undefined') {
       context.source[status.network] = context.known = true;
+      context.sourceName = status.network;
     }
 
     // handle the 'massrelevance' network type
     if (status.network === 'massrelevance') {
       context.source.message = context.known = true;
+      context.sourceName = 'message';
     }
 
     // for twitter, pull the retweeted status up and use it as the main status
