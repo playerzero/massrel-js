5837912538506b1f5c64bbabb11c5b9315c54010
Howard Rauscher <howardr@gmail.com>
2013-10-10 13:24:59 -0500

    rebuild

--- massrel.js
+++ massrel.js
@@ -797,8 +797,8 @@ massreljs.define('helpers',['./globals'], function(globals) {
     return wrapper;
   };
 
-  exports.timeParam = function(value, paramName, params) {
-    if(value) {
+  exports.timeParam = function(value, paramName, params, allowZeroOrNegative) {
+    if(typeof(value) !== 'undefined') {
       if(value.getTime) {
         value = value.getTime() / 1000;
       }
@@ -807,6 +807,9 @@ massreljs.define('helpers',['./globals'], function(globals) {
       if(!isNaN(value) && value > 0) {
         // bucket to closest minute
         value = Math.floor(value / 60) * 60;
+        params.push([paramName, value]);
+      }
+      else if(!isNaN(value) && allowZeroOrNegative && value <= 0) {
         params.push([paramName, value]);
       }
 
@@ -1487,7 +1490,99 @@ massreljs.define('stream_keyword_insights',['./helpers', './generic_poller'], fu
   return StreamKeywordInsights;
 });
 
-massreljs.define('stream',['./helpers', './poller', './meta_poller', './top_things_poller', './stream_keyword_insights'], function(helpers, Poller, MetaPoller, TopThingsPoller, StreamKeywordInsights) {
+massreljs.define('stream_activity',['./helpers', './generic_poller'], function(helpers, GenericPoller) {
+  var _enc = encodeURIComponent;
+
+  function StreamActivity(stream, defaults) {
+    this.stream = stream;
+    this.defaults = defaults || {};
+  };
+  StreamActivity.prototype.url = function() {
+    return helpers.api_url('/'+ _enc(this.stream.account) +'/'+ _enc(this.stream.stream_name) +'/activity.json');
+  };
+  StreamActivity.prototype.fetch = function(opts, fn, errback) {
+    opts = helpers.extend({}, opts || {});
+    opts = helpers.extend(opts, this.defaults);
+
+    var params = this.params(opts);
+    helpers.request_factory(this.url(), params, '_', this, function(data) {
+      if(data && (!opts.verbose || opts.verbose === '0') && helpers.is_array(data.activity)) {
+        var activity = data.activity;
+        for(var i = 0, len = activity.length; i < len; i++) {
+          var start = data.start + (data.period_size * i);
+          activity[i] = helpers.extend(activity[i], {
+            start: start,
+            finish: Math.min(start + data.period_size, data.finish)
+          });
+        }
+      }
+      if(typeof(fn) === 'function') {
+        fn.apply(this, arguments);
+      }
+    }, errback);
+    return this;
+  };
+  StreamActivity.prototype.poller = function(opts) {
+    var poller = new GenericPoller(this, opts);
+    poller.fetch = function(object, opts, cycle) {
+      return object.fetch(opts, cycle.callback, cycle.errback);
+    };
+
+    return poller;
+  };
+  StreamActivity.prototype.params = function(opts) {
+    opts = opts || {};
+    var params = [];
+
+    if(opts.view) {
+      params.push(['view', opts.view]);
+    }
+    else if(opts.topic) {
+      params.push(['topic', opts.topic]);
+    }
+    helpers.timeParam(opts.start, 'start', params, true);
+    helpers.timeParam(opts.finish, 'finish', params, true);
+    if('periods' in opts) {
+      params.push(['periods', opts.periods]);
+    }
+    if(opts.resolution) {
+      params.push(['resolution', opts.resolution]);
+    }
+    if('tz_offset' in opts) {
+      params.push(['tz_offset', opts.tz_offset]);
+    }
+    if(opts.encode) {
+      params.push(['encode', opts.encode]);
+    }
+    params.push(['verbose', 0]);
+
+    return params;
+  };
+
+  var predef = function(method, key, value) {
+    StreamActivity.prototype[method] = function() {
+      this.defaults[key] = value;
+      return this;
+    };
+  };
+
+  // view
+  predef('approved', 'view', 'approved');
+  predef('pending', 'view', 'pending');
+  predef('rejected', 'view', 'rejected');
+
+  // resolution
+  predef('minutes', 'resolution', '1m');
+  predef('ten_minutes', 'resolution', '10m');
+  predef('hours', 'resolution', '1h');
+  predef('days', 'resolution', '1d');
+
+
+  return StreamActivity;
+
+});
+
+massreljs.define('stream',['./helpers', './poller', './meta_poller', './top_things_poller', './stream_keyword_insights', './stream_activity'], function(helpers, Poller, MetaPoller, TopThingsPoller, StreamKeywordInsights, StreamActivity) {
   var _enc = encodeURIComponent;
 
   function Stream() {
@@ -1692,6 +1787,9 @@ massreljs.define('stream',['./helpers', './poller', './meta_poller', './top_thin
   };
   Stream.prototype.topThingsPoller = function(opts) {
     return new TopThingsPoller(this, opts);
+  };
+  Stream.prototype.activity = function(defaults) {
+    return new StreamActivity(this, defaults);
   };
 
   return Stream;
@@ -2016,6 +2114,8 @@ massreljs.define('massrel',[
        , './helpers'
        , './stream'
        , './account'
+       , './stream_keyword_insights'
+       , './stream_activity'
        , './generic_poller'
        , './generic_poller_cycle'
        , './poller'
@@ -2031,6 +2131,8 @@ massreljs.define('massrel',[
        , helpers
        , Stream
        , Account
+       , StreamKeywordInsights
+       , StreamActivity
        , GenericPoller
        , GenericPollerCycle
        , Poller
@@ -2046,6 +2148,8 @@ massreljs.define('massrel',[
   // public API
   massrel.Stream = Stream;
   massrel.Account = Account;
+  massrel.StreamKeywordInsights = StreamKeywordInsights;
+  massrel.StreamActivity = StreamActivity;
   massrel.GenericPoller = GenericPoller;
   massrel.GenericPollerCycle = GenericPollerCycle;
   massrel.Poller = Poller;
