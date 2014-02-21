8e87d0d0022af5565da7ca15d9714a7bb424bcd9
Howard Rauscher <howardr@gmail.com>
2014-02-21 00:28:26 -0600

    use grunt instead of rake for building

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,69 @@
 module.exports = function(grunt) {
+
+  var banner = [
+    '  /*!',
+    '   * <%= pkg.name %> <%= pkg.version %>',
+    '   *',
+    '   * Copyright <%= (new Date()).getYear() %> Mass Relevance',
+    '   *',
+    '   * Licensed under the Apache License, Version 2.0 (the "License");',
+    '   * you may not use this work except in compliance with the License.',
+    '   * You may obtain a copy of the License at:',
+    '   *',
+    '   *    http://www.apache.org/licenses/LICENSE-2.0',
+    '   */'
+  ].join('\n');
+
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: './src',
+          name: 'almond',
+          include: 'massrel',
+          namespace: 'massreljs',
+          skipModuleInsertion: true,
+          optimize: 'none',
+          out: 'build/massrel.js'
+        },
+      }
+    },
+    concat: {
+      build: {
+        src: [
+          'src/wrap.start.js',
+          'build/massrel.js',
+          'src/wrap.end.js'
+        ],
+        dest: 'build/massrel.js'
+      },
+      release: {
+        options: {
+          stripBanners: true,
+          banner: banner
+        },
+        files: {
+          'massrel.js': ['build/massrel.js'],
+          'massrel.min.js': ['build/massrel.min.js'],
+          'pkg/massrel.<%= pkg.version %>.js': ['build/massrel.js'],
+          'pkg/massrel.min.<%= pkg.version %>.js': ['build/massrel.min.js']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      build: {
+        src: 'build/massrel.js',
+        dest: 'build/massrel.min.js'
+      }
+    },
     jasmine: {
       components: {
         src: [
-          'massrel.js',
+          'build/massrel.js',
           'misc/*.js'
         ],
         options: {
@@ -17,5 +77,13 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['requirejs', 'concat:build', 'uglify', 'jasmine']);
+  grunt.registerTask('release', ['build', 'concat:release']);
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+
 };
